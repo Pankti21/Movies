fe54e0de1b9a3dcd5d2afcea5a9e9983f858c2b8
Pankti Vyas <pn695271@dal.ca>
2023-05-01 18:03:43 -0300

    API endpoints created.

--- movies/src/main/java/com/panktivyas/movies/MovieService.java
+++ movies/src/main/java/com/panktivyas/movies/MovieService.java
@@ -1,9 +1,11 @@
 package com.panktivyas.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -11,5 +13,11 @@ public class MovieService {
     private MovieRepository movieRepository;
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+
+    //The id passed might not exist. In this case it doesn't return a Movie but returns null.
+    //So the return type is Optional<Movie>
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
     }
 }
